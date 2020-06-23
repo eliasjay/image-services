1f2f6937fe4db269a738b3fcd2686591c8bdf945
eliasjay <zeliasgl@gmail.com>
2020-06-23 17:30:06 -0300

    error handler

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,30 @@
-import express from 'express'
+import 'reflect-metadata'
+
+import express, { Request, Response, NextFunction } from 'express'
+import 'express-async-errors'
+
+import cors from 'cors'
+import AppError from './shared/errors/AppError'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({message: 'it works'})
+app.use(cors())
+app.use(express.json())
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message
+    })
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
 })
 
 app.listen(3333)
