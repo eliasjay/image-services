565f474d6016da9e449845e77d41aa0048f13b9b
eliasjay <zeliasgl@gmail.com>
2020-11-10 16:10:27 -0300

    correcting .env names

--- src/modules/compressing/services/CompressingService.js
+++ src/modules/compressing/services/CompressingService.js
@@ -6,9 +6,9 @@ dotenv.config({ path: __dirname + '/.env' })
 class CompressingService {
   async execute(image) {
     cloudinary.config({
-      cloud_name: process.env.CLODINATY_NAME,
-      api_key: process.env.CLODINATY_API_KEY,
-      api_secret: process.env.CLODINATY_API_SECRET
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET
     });
 
     return await cloudinary.uploader.upload(
